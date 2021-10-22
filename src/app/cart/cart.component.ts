0ffd6ffee51d5512ade606e8ac372af826ae4f99
lagdush <zurawskiu@gmail.com>
2021-10-22 20:04:45 +0200

    feat: Cart view display items in cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
-export class CartComponent implements OnInit {
+export class CartComponent {
+  items = this.cartService.getItems();
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor(private cartService: CartService) {}
 }
